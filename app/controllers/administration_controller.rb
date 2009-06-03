ac7e28da08dcdb53df2cef7b1016704e59d45385
Peter Held <peter.t.held@gmail.com>
2009-06-02 22:52:44 -0700

    More view/controller stuff.

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -3,8 +3,8 @@ class AdministrationController < ApplicationController
   def index
     if logged_in?
       if current_user.is_admin?
-        @users = User.paginate :page => params[:user_page], :per_page => 2, :order => 'login ASC'
-        @activity_types = ActivityType.paginate :page => params[:at_page], :per_page => 2, :order => 'name ASC'
+        @users = User.paginate :page => params[:user_page], :per_page => User.per_page, :order => 'login ASC'
+        @activity_types = ActivityType.paginate :page => params[:at_page], :per_page => ActivityType.per_page, :order => 'name ASC'
 
         render :template => 'administration/index'
       else
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,13 +5,13 @@ class IndexController < ApplicationController
       # load stuff for the overview
       
       # get activities associated with the current user
-      @activities = Activity.paginate_by_user_id current_user.id, :page => params[:activity_page], :per_page => 2, :order => 'date DESC'
+      @activities = Activity.paginate_by_user_id current_user.id, :page => params[:activity_page], :per_page => Activity.per_page, :order => 'date DESC'
 
       # fitness samples associated with the current user
-      @fitness_samples = FitnessSample.paginate_by_user_id current_user.id, :page => params[:fs_page], :per_page => 2, :order => 'date DESC'
+      @fitness_samples = FitnessSample.paginate_by_user_id current_user.id, :page => params[:fs_page], :per_page => FitnessSample.per_page, :order => 'date DESC'
 
       # get the climbs associated with activities associated with the current user
-      @climbs = Climb.paginate_by_sql "SELECT c.* FROM climbs c, users u, activities a WHERE u.id = #{current_user.id.to_s} AND u.id = a.user_id AND a.id = c.activity_id ORDER BY a.date DESC", :page => params[:climb_page], :per_page => 2
+      @climbs = Climb.paginate_by_sql "SELECT c.* FROM climbs c, users u, activities a WHERE u.id = #{current_user.id.to_s} AND u.id = a.user_id AND a.id = c.activity_id ORDER BY a.date DESC", :page => params[:climb_page], :per_page => Climb.per_page
 
       render :template => 'index/index'
     else
