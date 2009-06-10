c25b3276e1ea0f8fbbf7cb5841cdb9e22f4322f4
Peter Held <pheld@peterheld-ubuntu.amer.gettywan.com>
2009-06-09 18:51:12 -0700

    Added fail images for when there are no samples yet.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -51,35 +51,40 @@ class IndexController < ApplicationController
 
     # get the fitness sample data sets for this user
     @fitness_samples = FitnessSample.find_all_by_user_id current_user.id, :order => 'date ASC'
-    weight_data_set = ghelper.fitness_samples_to_weight_data_set(@fitness_samples, first_date, last_date)
-    average_weight_data_set = ghelper.fitness_samples_to_seven_day_weight_average_data_set(@fitness_samples, first_date, last_date)
-    data_sets = []
-    data_sets << weight_data_set
-    data_sets << average_weight_data_set
+    if @fitness_samples.length > 0
+      weight_data_set = ghelper.fitness_samples_to_weight_data_set(@fitness_samples, first_date, last_date)
+      average_weight_data_set = ghelper.fitness_samples_to_seven_day_weight_average_data_set(@fitness_samples, first_date, last_date)
+      data_sets = []
+      data_sets << weight_data_set
+      data_sets << average_weight_data_set
 
-    graph = ghelper.generate_graph(data_sets, first_date, last_date)
+      graph = ghelper.generate_graph(data_sets, first_date, last_date)
 
-    send_data(graph.to_blob, :disposition => 'inline', :type => 'image/png', :filename => 'arbitraryfilename.png')
+      send_data(graph.to_blob, :disposition => 'inline', :type => 'image/png', :filename => 'arbitraryfilename.png')
+    else
+      send_file('public/images/zubaz-stallion.jpg', :disposition => 'inline', :type => 'image/jpeg')
+    end
   end
 
   def graph_body_fat_percentage
     ghelper = GraphHelper.new
 
-    puts first_date
-    puts last_date
-
     # get the fitness sample data sets for this user
     @fitness_samples = FitnessSample.find_all_by_user_id current_user.id, :order => 'date ASC'
-    bf_percent_data_set = ghelper.fitness_samples_to_body_fat_percentage_data_set(@fitness_samples, first_date, last_date)
-    average_bf_percent_data_set = ghelper.fitness_samples_to_seven_day_bfp_average_data_set(@fitness_samples, first_date, last_date)
-    data_sets = []
-    data_sets << bf_percent_data_set
-    data_sets << average_bf_percent_data_set
+    if @fitness_samples.length > 0
+      bf_percent_data_set = ghelper.fitness_samples_to_body_fat_percentage_data_set(@fitness_samples, first_date, last_date)
+      average_bf_percent_data_set = ghelper.fitness_samples_to_seven_day_bfp_average_data_set(@fitness_samples, first_date, last_date)
+      data_sets = []
+      data_sets << bf_percent_data_set
+      data_sets << average_bf_percent_data_set
 
-    graph = ghelper.generate_graph(data_sets, first_date, last_date)
-    graph.minimum_value = 0
+      graph = ghelper.generate_graph(data_sets, first_date, last_date)
+      graph.minimum_value = 0
 
-    send_data(graph.to_blob, :disposition => 'inline', :type => 'image/png', :filename => 'arbitraryfilename.png')
+      send_data(graph.to_blob, :disposition => 'inline', :type => 'image/png', :filename => 'arbitraryfilename.png')
+    else
+      send_file('public/images/zubaz-mesh-mullet.png', :disposition => 'inline', :type => 'image/png')
+    end
   end
 
   def graph_hours
@@ -87,30 +92,44 @@ class IndexController < ApplicationController
 
     # get the fitness sample data sets for this user
     @activities = Activity.find_all_by_user_id current_user.id, :order => 'date ASC'
-    total_hours_data_set = ghelper.activities_to_seven_day_hours_total_data_set(@activities, first_date, last_date)
-    data_sets = []
-    data_sets << total_hours_data_set
+    if @activities.length > 0
+      total_hours_data_set = ghelper.activities_to_seven_day_hours_total_data_set(@activities, first_date, last_date)
+      data_sets = []
+      data_sets << total_hours_data_set
 
-    graph = ghelper.generate_graph(data_sets, first_date, last_date)
-    graph.minimum_value = 0
+      graph = ghelper.generate_graph(data_sets, first_date, last_date)
+      graph.minimum_value = 0
 
-    send_data(graph.to_blob, :disposition => 'inline', :type => 'image/png', :filename => 'arbitraryfilename.png')
+      send_data(graph.to_blob, :disposition => 'inline', :type => 'image/png', :filename => 'arbitraryfilename.png')
+    else
+      send_file('public/images/zubaz-suit.jpg', :disposition => 'inline', :type => 'image/jpeg')
+    end
   end
 
   protected
 
   def first_date
     first_a_date = Activity.first_date_by_user(current_user.id)
-    first_fs_date = FitnessSample.first_date_by_user(current_user.id)
 
-    first_date = first_a_date <= first_fs_date ? first_a_date : first_fs_date
+    if FitnessSample.find_all_by_user_id(current_user.id).length > 0
+      first_fs_date = FitnessSample.first_date_by_user(current_user.id)
+
+      first_date = first_a_date <= first_fs_date ? first_a_date : first_fs_date
+    else
+      first_date = first_a_date
+    end
   end
 
   def last_date
     last_a_date = Activity.last_date_by_user(current_user.id)
-    last_fs_date = FitnessSample.last_date_by_user(current_user.id)
 
-    last_date = last_a_date >= last_fs_date ? last_a_date : last_fs_date
+    if FitnessSample.find_all_by_user_id(current_user.id).length > 0
+      last_fs_date = FitnessSample.last_date_by_user(current_user.id)
+
+      last_date = last_a_date >= last_fs_date ? last_a_date : last_fs_date
+    else
+      last_date = last_a_date
+    end
   end
 
 end
