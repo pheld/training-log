819b8dcbb7b96cf0dba4f50cc2460546bec17cae
Peter Held <pheld@pheldserv.local>
2011-02-08 09:56:23 -0800

    removed graph observer from environment.

--- config/environment.rb
+++ config/environment.rb
@@ -72,7 +72,6 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   #config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-  config.active_record.observers = :overview_graph_observer
  
   # Configure WillPaginate
   config.gem "mislav-will_paginate", :lib => 'will_paginate', :source => 'http://gems.github.com'
